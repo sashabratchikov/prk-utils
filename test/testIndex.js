a97c000fe9425d838ccabe4a94d43438cca5a8e2
Alexandr D. Kulikov <kulik-alex@yandex-team.ru>
2020-08-12 13:03:50 +0300

    jsxToJS

--- test/testIndex.js
+++ test/testIndex.js
@@ -194,3 +194,58 @@ describe('checkFiles', function () {
     assert.deepEqual(errors, expected);
   });
 });
+describe('jsxToJS', function () {
+  it('should translate jsx to js', function () {
+    const js = index.jsxToJS(`import React, { PureComponent, Component }  from 'react';
+    import { render } from 'react-dom';
+
+    class MySuperComponent extends PureComponent {
+      render() {
+        const { name } = this.props;
+        console.log(\`Имя: \${name}\`);
+
+        return <h1>{name}</h1>;
+      }
+    }
+
+    export default class App extends Component {
+      state = {
+        name: "Пупа",
+        dummy: false
+      };
+
+      toggleMessage = () => {
+        const { name } = this.state;
+
+        this.setState({ name: name === "Пупа" ? "Лупа" : "Пупа" });
+      };
+
+      toggleDummy = () => {
+        this.setState({ dummy: !this.state.dummy });
+      };
+
+      render() {
+        const { name } = this.state;
+
+        console.log("Рендер App");
+
+        return (
+          <React.Fragment>
+            <MySuperComponent name={name}/>
+
+            <button onClick={this.toggleMessage}>Изменить сообщение</button>
+            <button onClick={this.toggleDummy}>Сделать пустое изменение</button>
+          </React.Fragment>
+        );
+      }
+    }
+
+    const rootElement = document.getElementById("root");
+    render(<App/>, rootElement);`);
+    assert.include(js, `
+    _defineProperty(this, "state", {
+      name: "Пупа",
+      dummy: false
+    });`);
+  }).timeout(10_000);
+});
